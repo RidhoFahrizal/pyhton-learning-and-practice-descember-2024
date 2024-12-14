81f28eb057b28d30825a6038bf7429edba7d68c4
RidhoFahrizal <farizalridho@gmail.com>
2024-12-14 23:03:32 +0700

    membuat project tentang cerita menggunakan pyhton

---
+++ helloWorld.py
@@ -0,0 +1,26 @@
+
+input = input("Selamat datang di cerita fantasi, Ingin bermain ? \n")
+
+# ternary di python harus mengassign sebuah value ke variabel 
+# di python tidak ada variabel yang bertipe bool 
+
+isPlaying = 1 if input == 'y' or input == 'Y' else 0
+
+
+def mulai_cerita():
+    theme = print("Silahkan Pilih tema ceritamu")
+
+
+
+if(isPlaying):
+    mulai_cerita()
+else: 
+    print("oke, selamat tinggal")
+
+
+
+
+
+
+
+
